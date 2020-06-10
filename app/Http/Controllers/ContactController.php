91d160a40e1a352b5fdc28e90ba2974c0be11954
Hyunjin Yeo <jfluke1414@gmail.com>
2020-06-10 00:05:08 -0700

    update

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -16,17 +16,17 @@ class ContactController extends Controller
         $subject = $req->input('subject');
         $text = $req->input('message');
         $title = 'Aaron Portfolio Mail';
-       
+
         $data['name'] = $name;
         $data['email'] = $email;
         $data['subject'] = $subject;
         $data['text'] = $text;
         $data['title'] = $title;
-        
+
         Mail::send('emails.email', $data, function($message) {
             $message->to('jfluke1414@gmail.com', 'Aaron(Hyunjin Yeo)')->subject('From Aaron Portfolio page');
         });
-                
+
         if (Mail::failures()) {
 //             return response()->Fail('Sorry! Please try again.');
             echo "Failed";
@@ -34,16 +34,16 @@ class ContactController extends Controller
 //             return response()->success('Great! Successfully sended this E-mail to Aaron');
             echo "Sent";
         }
-        
+
 //         return $req->input();
-         
+
 //         $post = DB::insert('query');
-         
+
 //         if($post){
 //             $req = redirect('post')->with('success', "It has been updated");
 //         } else {
 //             $req = redirect('post/create')->with('dnager', 'input date failed');
 //         }
-                    
+
     }
 }
